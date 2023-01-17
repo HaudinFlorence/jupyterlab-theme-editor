be1b365f25878d9c56c4ec4cc953b3678e76c06a
Florence Haudin <haudin.florence@gmail.com>
2023-01-17 16:36:56 +0100

    Use the change of state of the model to set new values to the settings.

--- src/index.ts
+++ src/index.ts
@@ -9,35 +9,21 @@ import { ThemeEditorView } from './view';
 import { IChangedArgs } from '@jupyterlab/coreutils';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
+const PLUGIN_ID = 'jupyter-theme-editor:plugin';
+
 /**
  * Initialization data for the jupyter-theme-editor extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyter-theme-editor:plugin',
+  id: PLUGIN_ID,
   autoStart: true,
   requires: [IThemeManager],
   optional: [ISettingRegistry],
   activate: (
     app: JupyterFrontEnd,
     themeManager: IThemeManager,
-    settingRegistry: ISettingRegistry
+    settings: ISettingRegistry
   ) => {
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log(
-            'jupyter-theme-editor settings loaded:',
-            settings.composite
-          );
-        })
-        .catch(reason => {
-          console.error(
-            'Failed to load settings for jupyter-theme-editor.',
-            reason
-          );
-        });
-    }
     const onThemeChanged = (
       themeManager: IThemeManager,
       changes: IChangedArgs<string, string | null, string>
@@ -49,7 +35,52 @@ const plugin: JupyterFrontEndPlugin<void> = {
       view.id = 'theme-editor';
       view.title.icon = themeEditorIcon;
       app.shell.add(view, 'left');
+      console.log('JupyterLab extension jupyter-theme-editor is activated!');
+
+      /**
+       * Load the settings for this extension
+       *
+       * @param setting Extension settings
+       */
+
+      function loadSetting(setting: ISettingRegistry.ISettings): void {
+        // Read the settings and convert to the correct type
+        const settingsValues: any = {};
+        for (const key of Object.keys(model.formData)) {
+          if (key.includes('color') || key.includes('font-family')) {
+            settingsValues[key] = setting.get(key).composite as string;
+          } else {
+            settingsValues[key] = setting.get(key).composite as number;
+          }
+        }
+        return settingsValues;
+      }
+
+      // Wait for the application to be restored and
+      // for the settings for this plugin to be loaded
+
+      const onModelChanged = () => {
+        console.log('Model is changed ! We are using the theme parameters');
+        Promise.all([app.restored, settings.load(PLUGIN_ID)]).then(
+          ([, setting]) => {
+            // Read the settings
+            let settingsValues: any = {};
+            settingsValues = loadSetting(setting);
+            // Listen for your plugin setting changes using Signal
+            setting.changed.connect(loadSetting);
+            for (const key of Object.keys(model.formData)) {
+              if (settingsValues[key] !== model.formData[key]) {
+                console.log(`For key ${key}, set new value`);
+                setting.set(key, model.formData[key]);
+              }
+            }
+          }
+        );
+      };
+
+      model.stateChanged.connect(onModelChanged);
     };
+
     themeManager.themeChanged.connect(onThemeChanged);
   }
 };
